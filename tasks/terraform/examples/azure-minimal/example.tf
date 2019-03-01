9163f8ec0be844e2ebc8c3564347b817d27fe77b
Prasanth Pendham - ppendha <Prasanth.Pendham@walmart.com>
2019-02-28 19:06:41 -0600

    terraform: add a minimal Azure example (#5)

---
+++ tasks/terraform/examples/azure-minimal/example.tf
@@ -0,0 +1,16 @@
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
+
+provider "azurerm" {
+   client_id ="${var.client_id}"
+   client_secret = "${var.client_secret}"
+   subscription_id  = "${var.subscription_id}"
+   tenant_id = "${var.tenant_id}"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-azure"
+  location = "southcentralus"
+}
