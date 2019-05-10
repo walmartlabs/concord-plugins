8c31ab1e9b49dfaf336cb096d97d96da2b1fbd8e
Ivan Bodrov <ibodrov@gmail.com>
2019-05-10 08:25:21 -0400

    taurus: tidy up the example

--- tasks/taurus/examples/simple/README.md
+++ tasks/taurus/examples/simple/README.md
@@ -1,8 +1,6 @@
 # Running
 
-- create a Concord project. Enable "Allow payload archives" (not necessary for production usage);
-- update the credentials in `concord.yml`;
 - start the process by running:
   ```
-  $ ./run.sh http://concordhost:8001 orgName projectName
+  $ ./run.sh http://concordhost:8001
   ```
--- tasks/taurus/examples/simple/run.sh
+++ tasks/taurus/examples/simple/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 SERVER_ADDR="$1"
-ORG_NAME="$2"
-PROJECT_NAME="$3"
 
 rm -rf target && mkdir target
 cp concord.yml example.tf target/
@@ -10,4 +8,4 @@ cp concord.yml example.tf target/
 cd target && zip -r payload.zip ./* > /dev/null && cd ..
 
 read -p "Username: " CURL_USER
-curl -u ${CURL_USER} -F archive=@target/payload.zip -F org=${ORG_NAME} -F project=${PROJECT_NAME} http://${SERVER_ADDR}/api/v1/process
+curl -u ${CURL_USER} -F archive=@target/payload.zip http://${SERVER_ADDR}/api/v1/process
