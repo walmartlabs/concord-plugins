5d12f3bb7b846996c204274f3ca89b7c73d05c2e
Brigadirenko Yury <ybrigo@gmail.com>
2020-09-13 22:17:05 +0300

    ldap: runtime-v2 support

--- tasks/ldap/src/main/java/com/walmartlabs/concord/plugins/ldap/LdapTask.java
+++ tasks/ldap/src/main/java/com/walmartlabs/concord/plugins/ldap/LdapTask.java
@@ -20,50 +20,17 @@ package com.walmartlabs.concord.plugins.ldap;
  * =====
  */
 
+import com.walmartlabs.concord.runtime.v2.sdk.Variables;
 import com.walmartlabs.concord.sdk.Context;
-import com.walmartlabs.concord.sdk.ContextUtils;
 import com.walmartlabs.concord.sdk.InjectVariable;
 import com.walmartlabs.concord.sdk.Task;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Named;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
-import javax.naming.directory.Attribute;
-import javax.naming.directory.Attributes;
-import javax.naming.directory.SearchControls;
-import javax.naming.directory.SearchResult;
-import javax.naming.ldap.InitialLdapContext;
-import javax.naming.ldap.LdapContext;
-import javax.net.ssl.SSLHandshakeException;
-import java.util.*;
-import java.util.concurrent.Callable;
+import java.util.Map;
 
-import static com.walmartlabs.concord.sdk.ContextUtils.assertString;
-
-/**
- * Created by ppendha on 6/18/18.
- */
 @Named("ldap")
+@SuppressWarnings("unused")
 public class LdapTask implements Task {
-
-    private static final Logger log = LoggerFactory.getLogger(LdapTask.class);
-
-    private static final int MAX_RETRIES = 3;
-    private static final long RETRY_DELAY = 3000;
-
-    private static final String ACTION_KEY = "action";
-
-    // in params
-    private static final String LDAP_AD_SERVER = "ldapAdServer";
-    private static final String LDAP_BIND_USER_DN = "bindUserDn";
-    private static final String LDAP_BIND_PASSWORD = "bindPassword";
-    private static final String LDAP_SEARCH_BASE = "searchBase";
-    private static final String LDAP_USER = "user";
-    private static final String LDAP_GROUP = "group";
-    private static final String LDAP_SECURITY_ENABLED = "securityEnabled";
-    private static final String LDAP_DN = "dn";
 
     // out params
     private static final String LDAP_OUT = "out";
@@ -74,335 +41,45 @@ public class LdapTask implements Task {
 
     @Override
     public void execute(Context ctx) {
-        Action action = getAction(ctx);
+        Map<String, Object> result = new LdapTaskCommon()
+                .execute(TaskParams.of(new ContextVariables(ctx), defaults));
 
-        switch (action) {
-            case SEARCHBYDN: {
-                log.info("Starting 'SearchByDn' Action");
-                searchByDn(ctx, null);
-                break;
-            }
-            case GETUSER: {
-                log.info("Starting 'GetUser' Action");
-                getUser(ctx);
-                break;
-            }
-            case GETGROUP: {
-                log.info("Starting 'GetGroup' Action");
-                getGroup(ctx);
-                break;
-            }
-            case ISMEMBEROF: {
-                log.info("Starting 'IsMemberOf' Action");
-                isMemberOf(ctx);
-                break;
-            }
-            default:
-                throw new IllegalArgumentException("Unsupported action type: " + action);
-        }
+        String key = getString(null, ctx, LDAP_OUT, LDAP_DEFAULT_OUT);
+
+        ctx.setVariable(key, result);
     }
 
     public boolean isMemberOf(@InjectVariable("context") Context ctx, String userDn, String groupDn) {
-        SearchResult result = searchByDn(ctx, groupDn);
-        if (result == null) {
-            return false;
-        }
-
-        NamingEnumeration<String> members = getAttrValues(result, "member");
-        if (members != null) {
-            while (members.hasMoreElements()) {
-                String member = members.nextElement();
-                if (Objects.equals(userDn, member) || isMemberOf(ctx, userDn, member)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return new LdapTaskCommon().isMemberOf(TaskParams.searchParams(new ContextVariables(ctx), defaults), userDn, groupDn);
     }
 
-    private SearchResult searchByDn(Context ctx, String dn) {
-        if (dn == null) {
-            dn = assertString(ctx, LDAP_DN);
+    private static class ContextVariables implements Variables {
+
+        private final Context context;
+
+        public ContextVariables(Context context) {
+            this.context = context;
         }
 
-        boolean success = false;
-        SearchResult result = null;
-
-        try {
-            // create custom filter for dn
-            String searchFilter = "(distinguishedName=" + dn + ")";
-
-            // use private method search
-            NamingEnumeration<SearchResult> results = withRetry(MAX_RETRIES, RETRY_DELAY, () -> search(ctx, searchFilter));
-
-            if (results.hasMoreElements()) {
-                result = results.nextElement();
-                success = true;
-            }
-
-            setOutVariable(ctx, success, searchResultToMap(result));
-            return result;
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Error occurred while searching " + e);
-        }
-    }
-
-    private SearchResult getUser(Context ctx) {
-        String user = assertString(ctx, LDAP_USER);
-
-        boolean success = false;
-        SearchResult result = null;
-
-        try {
-            // create custom filter for user
-            String searchFilter = "(|"
-                    + "(userPrincipalName=" + user + ")"
-                    + "(sAMAccountName=" + user + ")"
-                    + "(mailNickname=" + user + ")"
-                    + "(proxyAddresses=smtp:" + user + ")"
-                    + "(mail=" + user + ")"
-                    + ")";
-
-            // use private method search
-            NamingEnumeration<SearchResult> results = withRetry(MAX_RETRIES, RETRY_DELAY, () -> search(ctx, searchFilter));
-
-            if (results.hasMoreElements()) {
-                result = results.nextElement();
-                success = true;
-            }
-
-            setOutVariable(ctx, success, searchResultToMap(result));
-            return result;
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Error occurred while searching " + e);
-        }
-    }
-
-    private SearchResult getGroup(Context ctx) {
-        String group = assertString(ctx, LDAP_GROUP);
-        boolean securityEnabled = ContextUtils.assertVariable(ctx, LDAP_SECURITY_ENABLED, Boolean.class);
-
-        boolean success = false;
-        SearchResult result = null;
-
-        try {
-            // create custom filter for group
-            String searchFilter = "(name=" + group + ")";
-
-            // use private method search
-            NamingEnumeration<SearchResult> results = withRetry(MAX_RETRIES, RETRY_DELAY, () -> search(ctx, searchFilter));
-
-            while (results.hasMoreElements()) {
-                result = results.nextElement();
-                String dn = getAttrValue(result, "distinguishedName");
-
-                if (dn != null && dn.toLowerCase().contains("ou=security") == securityEnabled) {
-                    success = true;
-                    break;
-                }
-            }
-
-            setOutVariable(ctx, success, searchResultToMap(result));
-            return result;
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Error occurred while searching " + e);
-        }
-    }
-
-    private void isMemberOf(Context ctx) {
-        boolean success = false;
-        boolean result = false;
-
-        try {
-            SearchResult user = getUser(ctx);
-            SearchResult group = getGroup(ctx);
-
-            if (user != null && group != null) {
-                String userDn = getAttrValue(user, "distinguishedName");
-                String groupDn = getAttrValue(group, "distinguishedName");
-
-                result = isMemberOf(ctx, userDn, groupDn);
-                success = true;
-            }
-
-            setOutVariable(ctx, success, result);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Error occurred while searching " + e);
-        }
-    }
-
-    private NamingEnumeration<SearchResult> search(Context ctx, String searchFilter) {
-        String searchBase = assertString(ctx, LDAP_SEARCH_BASE);
-        LdapContext connection = null;
-        try {
-            connection = establishConnection(ctx);
-            SearchControls searchControls = new SearchControls();
-            searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
-
-            // create SearchRequest
-            return withRetry(MAX_RETRIES, RETRY_DELAY, () -> establishConnection(ctx).search(searchBase, searchFilter, searchControls));
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Error occurred while searching " + e);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (NamingException e) {
-                    throw new IllegalArgumentException("Error occurred while closing connection " + e);
-                }
-            }
-        }
-    }
-
-    private LdapContext establishConnection(Context ctx) {
-        String ldapAdServer = getString(defaults, ctx, LDAP_AD_SERVER, null);
-        String bindUserDn = getString(defaults, ctx, LDAP_BIND_USER_DN, null);
-        String bindPassword = getString(defaults, ctx, LDAP_BIND_PASSWORD, null);
-
-        Hashtable<String, Object> env = new Hashtable<String, Object>();
-        env.put(javax.naming.Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(javax.naming.Context.PROVIDER_URL, ldapAdServer);
-        env.put(javax.naming.Context.SECURITY_AUTHENTICATION, "simple");
-        env.put(javax.naming.Context.SECURITY_PRINCIPAL, bindUserDn);
-        env.put(javax.naming.Context.SECURITY_CREDENTIALS, bindPassword);
-        env.put("java.naming.ldap.version", "3");
-
-        try {
-            return new InitialLdapContext(env, null);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Error while establishing connection " + e);
-        }
-    }
-
-    private static String getAttrValue(SearchResult result, String id) {
-        NamingEnumeration<String> values = getAttrValues(result, id);
-
-        if (values.hasMoreElements()) {
-            return values.nextElement();
+        @Override
+        public Object get(String key) {
+            return context.getVariable(key);
         }
 
-        return null;
-    }
-
-    @SuppressWarnings("unchecked")
-    private static NamingEnumeration<String> getAttrValues(SearchResult result, String id) {
-        if (result != null) {
-            Attributes attributes = result.getAttributes();
-            if (attributes != null) {
-                Attribute attribute = attributes.get(id);
-                if (attribute != null) {
-                    try {
-                        return (NamingEnumeration<String>) attribute.getAll();
-                    } catch (NamingException e) {
-                        return null;
-                    }
-                }
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Converts a {@link SearchResult} object to a Map
-     *
-     * @param r object to convert to a Map
-     * @return Map representation of the SearchResult object
-     */
-    private static Map<String, Object> searchResultToMap(SearchResult r) {
-        if (r == null) {
-            return null;
+        @Override
+        public void set(String key, Object value) {
+            throw new IllegalStateException("Unsupported");
         }
 
-        Map<String, Object> rMap = new HashMap<>(r.getAttributes().size());
-        try {
-            Attributes attrs = r.getAttributes();
-            NamingEnumeration<String> e = attrs.getIDs();
-
-            Map<String, Object> attrsMap = new HashMap<>(attrs.size());
-            while (e.hasMore()) {
-                String key = e.next();
-                Attribute attribute = attrs.get(key);
-
-                Set<String> values = getAllAttributesValues(attribute);
-                if (values.size() == 1) {
-                    attrsMap.put(key, values.iterator().next());
-                } else {
-                    attrsMap.put(key, values);
-                }
-
-            }
-            rMap.put("attributes", attrsMap);
-        } catch (Exception ex) {
-            log.error("Error mapping SearchResult attributes: {}", ex.getMessage());
+        @Override
+        public boolean has(String key) {
+            return context.getVariable(key) != null;
         }
 
-        return rMap;
-    }
-
-    /**
-     * Executes a Callable until successful, up to a given number of retries
-     *
-     * @param retryCount    Number of allowed retry attempts
-     * @param retryInterval Milliseconds to wait between retries
-     * @param c             Callable to execute
-     * @param <T>           Type of Callable
-     * @throws RuntimeException when api call can't be made successfully
-     */
-    private static <T> T withRetry(int retryCount, long retryInterval, Callable<T> c) throws Exception {
-        Exception exception = null;
-        int tryCount = 0;
-        while (!Thread.currentThread().isInterrupted() && tryCount <= retryCount) {
-            if (tryCount > 0) {
-                log.info("Retry after {} sec", retryInterval / 1000);
-                sleep(retryInterval);
-                log.info("Retrying...");
-            }
-            try {
-                return c.call(); // execute it
-            } catch (SSLHandshakeException e) {
-                // probably due to self-signed cert that isn't trusted
-                log.error("Error during SSL handshake; possibly due to untrusted self-signed certificate." + e.getMessage());
-                throw e;
-            } catch (Exception e) {
-                exception = e;
-                log.error("call error", e);
-            }
-            tryCount++;
+        @Override
+        public Map<String, Object> toMap() {
+            return context.toMap();
         }
-
-        if (Thread.currentThread().isInterrupted()) {
-            throw new InterruptedException();
-        }
-
-        // too many attempts, time to give up
-        throw new RuntimeException(exception);
-    }
-
-
-    private static Set<String> getAllAttributesValues(Attribute attribute) throws NamingException {
-        Set<String> values = new HashSet<>();
-
-        NamingEnumeration ne = attribute.getAll();
-        while (ne.hasMore()) {
-            Object o = ne.next();
-            values.add(o.toString());
-        }
-
-        return values;
-    }
-
-    private static void setOutVariable(Context ctx, Boolean success, Object result) {
-        String key = getString(null, ctx, LDAP_OUT, LDAP_DEFAULT_OUT);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("success", success);
-        response.put("result", result);
-
-        ctx.setVariable(key, response);
-    }
-
-    private static Action getAction(Context ctx) {
-        return Action.valueOf(assertString(ctx, ACTION_KEY).trim().toUpperCase());
     }
 
     private static String getString(Map<String, Object> defaults, Context ctx, String k, String defaultValue) {
@@ -429,20 +106,5 @@ public class LdapTask implements Task {
         }
 
         return v;
-    }
-
-    static void sleep(long t) {
-        try {
-            Thread.sleep(t);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    private enum Action {
-        SEARCHBYDN,
-        GETUSER,
-        GETGROUP,
-        ISMEMBEROF
     }
 }
---
+++ tasks/ldap/src/main/java/com/walmartlabs/concord/plugins/ldap/v2/LdapTaskV2.java
@@ -0,0 +1,57 @@
+package com.walmartlabs.concord.plugins.ldap.v2;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2020 Walmart Inc., Concord Authors
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import com.walmartlabs.concord.plugins.ldap.LdapSearchParams;
+import com.walmartlabs.concord.plugins.ldap.LdapTaskCommon;
+import com.walmartlabs.concord.plugins.ldap.TaskParams;
+import com.walmartlabs.concord.runtime.v2.sdk.*;
+import com.walmartlabs.concord.sdk.MapUtils;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Collections;
+import java.util.Map;
+
+@Named("ldap")
+public class LdapTaskV2 implements Task {
+
+    private final Context context;
+
+    private final LdapTaskCommon delegate = new LdapTaskCommon();
+
+    @Inject
+    public LdapTaskV2(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public TaskResult execute(Variables input) {
+        Map<String, Object> result = delegate.execute(TaskParams.of(input, context.defaultVariables().toMap()));
+        return new TaskResult(MapUtils.getBoolean(result, "success", false))
+                .values(result);
+    }
+
+    public boolean isMemberOf(String userDn, String groupDn) {
+        LdapSearchParams searchParams = TaskParams.searchParams(new MapBackedVariables(Collections.emptyMap()), context.defaultVariables().toMap());
+        return new LdapTaskCommon().isMemberOf(searchParams, userDn, groupDn);
+    }
+}
